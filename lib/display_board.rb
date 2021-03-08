963463126416cc4dff3c62c22cf2af1a7f685561
Daysha Jones <daysha.vu@icloud.com>
2021-03-08 22:06:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,13 +5,10 @@
 def display_board(board)
   puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
-  puts "#{board[3]}|#{board[2]}|#{board[6]}"
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "-----------"
-  puts "#{board[8]}|#{board[9]}|#{board[10]}"
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
-
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board)
