908f36bab21ab591cd4aa20b74e33560f9a7b725
Daysha Jones <daysha.vu@icloud.com>
2021-03-08 21:55:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,13 @@
 # out the current state.
 
 
-# def display_board
-#   puts "   |   |   "
-#   puts "-----------"
-#   puts "   |   |   "
-#   puts "-----------"
-#   puts "   |   |   "
-# end
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
 
 board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
